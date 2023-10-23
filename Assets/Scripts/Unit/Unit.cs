12d9445562c7e46787fec1021f953580e55b67c7
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2023-10-24 04:21:40 +0600

    Generic Action

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -15,9 +15,6 @@ public class Unit : MonoBehaviour
 
     private GridPosition gridPosition;
     private HealthSystem healthSystem;
-    private MoveAction moveAction;
-    private SpinAction spinAction;
-    private ShootAction shootAction;
     private BaseAction[] baseActionArray;
 
     private int actionPoints = ACTION_POINT_MAX;
@@ -25,9 +22,6 @@ public class Unit : MonoBehaviour
     private void Awake()
     {
         healthSystem = GetComponent<HealthSystem>();
-        moveAction = GetComponent<MoveAction>();
-        spinAction = GetComponent<SpinAction>();
-        shootAction = GetComponent<ShootAction>();
         baseActionArray = GetComponents<BaseAction>();
     }
 
@@ -77,11 +71,17 @@ public class Unit : MonoBehaviour
 
     public Vector3 GetWorldPosition() => transform.position;
 
-    public MoveAction GetMoveAction() => moveAction;
-
-    public SpinAction GetSpinAction() => spinAction;
-
-    public ShootAction GetShootAction() => shootAction;
+    public T GetAction<T>() where T : BaseAction
+    {
+        foreach(BaseAction baseAction in baseActionArray)
+        {
+            if (baseAction is T)
+            {
+                return (T)baseAction;
+            }
+        }
+        return null;
+    }
 
     public BaseAction[] GetBaseActionArray() => baseActionArray;
 
